46e6744ad0abc7ef4ecc69ee3ddcfa2e1a32906c
Idan Arbel <idan@arbel-deisngs.com>
2012-11-23 16:38:51 +0200

    ruby

---
+++ tree/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tree::Application.config.secret_token = 'ec7644b068bc7722fe62abc96042f6d379e7f7713d8827aa4031f27b637fe5153548838cef97514cdce582266b81f177c28ff4bd7558f27a393ac330cb104f52'
